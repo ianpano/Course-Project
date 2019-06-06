e58d1551d2286898672bd8073342c4ff039d81e0
ianpano <ianp@magenic.com>
2019-06-06 13:01:03 +0800

    caybiga-june06-1pm

---
+++ src/app/shopping/shopping-list-edit/shopping-list-edit.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-shopping-list-edit-component',
+    templateUrl: './shopping-list-edit.component.html',
+})
+export class ShoppingListEditComponent {
+    title = 'Shopping List Edit Works!';
+}
